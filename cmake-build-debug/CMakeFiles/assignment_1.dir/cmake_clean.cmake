7138d0c484fbcf74be7ac8e21d6ca82885c2c90e
Hemdan47 <mohammedahmed98731@gmail.com>
2023-10-08 19:07:23 +0300

    modified the writing and reading functions

---
+++ cmake-build-debug/CMakeFiles/assignment_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment_1.dir/bmplib.cpp.obj"
+  "CMakeFiles/assignment_1.dir/bmplib.cpp.obj.d"
+  "CMakeFiles/assignment_1.dir/main.cpp.obj"
+  "CMakeFiles/assignment_1.dir/main.cpp.obj.d"
+  "assignment_1.exe"
+  "assignment_1.exe.manifest"
+  "assignment_1.pdb"
+  "libassignment_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
+#include "bmplib.h"
+#include <unistd.h>
+using namespace std;
+unsigned char imgGS[SIZE][SIZE];
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string image_name,path = "\\imgs\\";
+    cout << "Enter Image name (NOTE: Must be in imgs folder): ";
+    cin >> image_name;
+    path += image_name;
+    path += ".bmp";
+    char cwd[PATH_MAX];
+    readGSBMP(strcat(getcwd(cwd,sizeof(cwd)),path.c_str()),imgGS);
+    showGSBMP(imgGS);
 }
